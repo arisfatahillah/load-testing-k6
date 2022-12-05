2e7dce3ef7bcd9ff896760b628d43f2c5ea414cf
arisfatahillah <neymardona543@gmail.com>
2022-12-05 22:18:03 +0700

    Update scripts.js
    add script

--- scripts.js
+++ scripts.js
@@ -1,1 +1,33 @@
 
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+
+export let options = {
+    vus: 10,
+    duration: '120s',
+};
+
+export default function () {
+  const url = 'https://reqres.in/api/register';
+  
+  const payload = JSON.stringify({
+    email: 'eve.holt@reqres.in',
+    password: 'pistol'
+  });
+
+  const params = {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+  };
+
+  let res = http.post(url, payload, params);
+
+  console.log(res.body);
+
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+  });
+
+}
